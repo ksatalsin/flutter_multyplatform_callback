8bb901af3b2f90d1db5d9c860245320cd1d0f986
ksatalsin <ksatalsin@gmail.com>
2019-10-07 11:28:56 +0300

    added tensorflow

--- android/app/src/main/java/com/kotlinflutter/flutterapp/SecondActivity.kt
+++ android/app/src/main/java/com/kotlinflutter/flutterapp/SecondActivity.kt
@@ -1,9 +1,10 @@
 package com.kotlinflutter.flutterapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
-
+import kotlinx.android.synthetic.main.content_second.cameraBtn
 
 class SecondActivity : AppCompatActivity() {
 
@@ -16,9 +17,15 @@ class SecondActivity : AppCompatActivity() {
         val actionBar = supportActionBar
 
         // Set toolbar title/app title
-        actionBar!!.title = "Android Component"
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar?.title = "Android Component"
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.setDisplayShowHomeEnabled(true)
+
+        cameraBtn?.setOnClickListener {
+
+
+            startActivity(Intent(this, CameraActivity::class.java))
+        }
 
     }
 
